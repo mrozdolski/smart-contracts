555e89c4a2f76e32069860a2083b2375d1a2bb0b
Mateusz Rozdolski <mateusz.rozdolski@sw3.io>
2022-11-22 19:56:43 +0100

    add

---
+++ contracts/NFTMint.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTMint is ERC721, Ownable {
+
+    uint public mintPrice = 0.05 ether;
+    uint public totalSupply;
+    uint public maxSupply;
+    bool public isMintEnabled;
+
+    mapping (address => uint) public mintedWallets;
+
+    constructor() payable ERC721("NFTMint", "NFTMINT") {
+        maxSupply = 2;
+    }
+
+    function toggleIsMintEnabled() external onlyOwner {
+        isMintEnabled = !isMintEnabled;
+    }
+
+    function setMaxSupply(uint _maxSupply) external onlyOwner {
+        maxSupply = _maxSupply;
+    }
+
+    function mint() external payable {
+        require(isMintEnabled, "minting not enabled" );
+        require(mintedWallets[msg.sender] < 1, "Exceeds max per wallet");
+        require(msg.value == mintPrice, "wrong value");
+        require(maxSupply > totalSupply, "sold out");
+
+        mintedWallets[msg.sender]++;
+        totalSupply++;
+        uint tokenId = totalSupply;
+        _safeMint(msg.sender, tokenId);
+    }
+    
+}
+
